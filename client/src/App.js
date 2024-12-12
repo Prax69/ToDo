41b3b31647e8ad99a17a079481711dc954073afb
Pratham Sagar <prathamsagar03@gmail.com>
2024-12-12 12:45:15 +0530

    Refreshing

--- client/src/App.js
+++ client/src/App.js
@@ -3,28 +3,19 @@ import Home from "./components/Home.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from "./components/Register.jsx";
 import Login from "./components/Login.jsx";
-// import { useState } from "react";
-// import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 // import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-
 function App() {
-
-
   return (
     <>
       <BrowserRouter>
         {/* <Header /> */}
         <Routes>
-          <Route path="/" element={<Home  />} />
-          <Route
-            path="/register"
-            element={<Register  />}
-          />
-          <Route
-            path="/login"
-            element={<Login />}
-          />
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </>
